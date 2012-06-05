7dbf60f86edbd17c7a3b7608a2228f31695f1908
Matthew Pocock <turingatemyhamster@gmail.com>
2012-06-06 00:07:17 +0100

    The document implementation class now is an SBOL resolver which makes available all entities in the document.

--- src/org/sbolstandard/core/impl/SBOLDocumentImpl.java
+++ src/org/sbolstandard/core/impl/SBOLDocumentImpl.java
@@ -15,6 +15,8 @@
 
 package org.sbolstandard.core.impl;
 
+import java.beans.Transient;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,9 +27,8 @@ import javax.xml.bind.annotation.XmlElements;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
-import org.sbolstandard.core.SBOLDocument;
-import org.sbolstandard.core.SBOLRootObject;
-import org.sbolstandard.core.SBOLVisitor;
+import org.sbolstandard.core.*;
+import org.sbolstandard.core.util.SBOLBaseVisitor;
 
 
 /**
@@ -39,7 +40,7 @@ import org.sbolstandard.core.SBOLVisitor;
     "rootObjects"
 })
 @XmlRootElement(name = "RDF", namespace = "http://www.w3.org/1999/02/22-rdf-syntax-ns#")
-public class SBOLDocumentImpl extends SBOLVisitableImpl implements SBOLDocument {
+public class SBOLDocumentImpl extends SBOLVisitableImpl implements SBOLDocument, SbolResolver {
     @XmlElements({
         @XmlElement(name = "Collection", type = CollectionImpl.class),
         @XmlElement(name = "DnaComponent", type = DnaComponentImpl.class),
@@ -88,5 +89,137 @@ public class SBOLDocumentImpl extends SBOLVisitableImpl implements SBOLDocument 
     @Override
     public void accept(SBOLVisitor visitor) {
 	    visitor.visit(this);
+    }
+
+    @Override
+    @Transient
+    public UriResolver<Collection> getCollectionUriResolver() {
+        return new UriResolver<Collection>() {
+            @Override
+            public Collection resolve(final URI uri) {
+                final List<Collection> found = new ArrayList<Collection>();
+
+                accept(new SBOLBaseVisitor() {
+                    @Override
+                    public void visit(Collection coll) {
+                        if(coll.getURI().equals(uri))
+                            found.add(coll);
+                    }
+                });
+
+                // fixme: should merge these
+                return found.isEmpty() ? null : found.get(0);
+            }
+        };
+    }
+
+    @Override
+    @Transient
+    public UriResolver<DnaComponent> getComponentUriResolver() {
+        return new UriResolver<DnaComponent>() {
+            @Override
+            public DnaComponent resolve(final URI uri) {
+                final List<DnaComponent> found = new ArrayList<DnaComponent>();
+
+                accept(new SBOLBaseVisitor() {
+                    @Override
+                    public void visit(DnaComponent dc) {
+                        if(dc.getURI().equals(uri))
+                            found.add(dc);
+                    }
+                });
+
+                // fixme: should merge these
+                return found.isEmpty() ? null : found.get(0);
+            }
+        };
+    }
+
+    @Override
+    @Transient
+    public UriResolver<DnaSequence> getSequenceUriResolver() {
+        return new UriResolver<DnaSequence>() {
+            @Override
+            public DnaSequence resolve(final URI uri) {
+                final List<DnaSequence> found = new ArrayList<DnaSequence>();
+
+                accept(new SBOLBaseVisitor() {
+                    @Override
+                    public void visit(DnaSequence ds) {
+                        if(ds.getURI().equals(uri))
+                            found.add(ds);
+                    }
+                });
+
+                // fixme: should merge these
+                return found.isEmpty() ? null : found.get(0);
+            }
+        };
+    }
+
+    @Override
+    @Transient
+    public UriResolver<SequenceAnnotation> getAnnotationUriResolver() {
+        return new UriResolver<SequenceAnnotation>() {
+            @Override
+            public SequenceAnnotation resolve(final URI uri) {
+                final List<SequenceAnnotation> found = new ArrayList<SequenceAnnotation>();
+
+                accept(new SBOLBaseVisitor() {
+                    @Override
+                    public void visit(SequenceAnnotation annotation) {
+                        if(annotation.getURI().equals(uri))
+                            found.add(annotation);
+                    }
+                });
+
+                // fixme: should merge these
+                return found.isEmpty() ? null : found.get(0);
+            }
+        };
+    }
+
+    @Override
+    @Transient
+    public DisplayIdResolver<Collection> getCollectionDisplayIdResolver() {
+        return new DisplayIdResolver<Collection>() {
+            @Override
+            public Collection resolve(final String displayId) {
+                final List<Collection> found = new ArrayList<Collection>();
+
+                accept(new SBOLBaseVisitor() {
+                    @Override
+                    public void visit(Collection coll) {
+                        if(coll.getDisplayId().equals(displayId))
+                            found.add(coll);
+                    }
+                });
+
+                // fixme: should merge these
+                return found.isEmpty() ? null : found.get(0);
+            }
+        };
+    }
+
+    @Override
+    @Transient
+    public DisplayIdResolver<DnaComponent> getComponentDisplayIdResolver() {
+        return new DisplayIdResolver<DnaComponent>() {
+            @Override
+            public DnaComponent resolve(final String displayId) {
+                final List<DnaComponent> found = new ArrayList<DnaComponent>();
+
+                accept(new SBOLBaseVisitor() {
+                    @Override
+                    public void visit(DnaComponent component) {
+                        if(component.getDisplayId().equals(displayId))
+                            found.add(component);
+                    }
+                });
+
+                // fixme: should merge these
+                return found.isEmpty() ? null : found.get(0);
+            }
+        };
     }
 }
