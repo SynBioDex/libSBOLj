77bbb61fbe5c1fb8dd9aee420a072d582608591c
Tramy <tramy.nguy@gmail.com>
2014-12-10 11:36:19 -0700

    Implemented all TopLevel SBOLReader. Still need to update new changes from the updated UML diagram with orientation, location, and ranges. Parsing internal NestedDocument still needs to be corrected. Lastly, instead of looping through all NamedProperty multiple times, call constructor instead of calling .create()

--- core2/src/main/java/org/sbolstandard/core2/AccessType.java
+++ core2/src/main/java/org/sbolstandard/core2/AccessType.java
@@ -1,15 +1,16 @@
 package org.sbolstandard.core2;
 
 public enum AccessType {
-	
+
 	PUBLIC("public"),
 	PRIVATE("private");
-	
+
 	private String accessTypeAlias;
-	
+
 	private AccessType(String accessTypeAlias) {
 		this.accessTypeAlias = accessTypeAlias;
 	}
+
 
 	/**
 	 * Returns the access type ("public" or "private").
