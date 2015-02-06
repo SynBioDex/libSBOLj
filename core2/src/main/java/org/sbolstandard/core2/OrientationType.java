21e49eb96f9559ed4f45e2534f4c5cd72f13f9ee
Zhen Zhang <moschino21@gmail.com>
2015-02-06 00:22:16 -0700

    Renamed Orientation to OrientationType. Added methods in AccessType. Restored DirectionType as Enum.

--- core2/src/main/java/org/sbolstandard/core2/OrientationType.java
+++ core2/src/main/java/org/sbolstandard/core2/OrientationType.java
@@ -6,6 +6,12 @@ package org.sbolstandard.core2;
  * @author Nicholas Roehner
  * @version 2.0
  */
+public enum OrientationType {
+	inline,
+	reverseComplement
+}
+
+
 //public enum Orientation {
 //	inline("inline"), 
 //	reverseComplement("reverseComplement"),
