83db1a081d1505a57679d546c6774a8e649c3b0a
Tramy <tramy.nguy@gmail.com>
2015-03-20 12:41:25 -0600

    added JUnit test files.

---
+++ core2/src/test/java/org/sbolstandard/core2/GenerateTestFile.java
@@ -0,0 +1,31 @@
+package org.sbolstandard.core2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URI;
+
+public class GenerateTestFile
+{
+
+	public static void main(String[] args)
+	{
+		SBOLDocument document = new SBOLDocument();
+
+		document.addNameSpaceBinding(URI.create("http://myannotation.org"), "annot");
+		document.addNameSpaceBinding(URI.create("urn:bbn.com:tasbe:grn"), "grn");
+		SBOLTestUtils.createGenericTopLevel(document, "GenericTopLevel");
+
+		writeRdfFile(document, "singleGenericTopLevel.rdf");
+
+	}
+
+	public static void writeRdfFile(SBOLDocument document, String fileName)
+	{
+		try {
+			SBOLWriter.writeRdf(document, new File(fileName));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
