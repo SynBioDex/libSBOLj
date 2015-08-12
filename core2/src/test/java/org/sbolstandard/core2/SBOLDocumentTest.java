d0b6dced6fd50706926bab012a9e0f8850fb8c75
EGC1995 <eugene.echoe@gmail.com>
2015-08-12 13:02:21 -0600

    Additions to CopyCreate() test methods in SBOLDocumentTest.java

--- core2/src/test/java/org/sbolstandard/core2/SBOLDocumentTest.java
+++ core2/src/test/java/org/sbolstandard/core2/SBOLDocumentTest.java
@@ -52,7 +52,10 @@ public class SBOLDocumentTest {
 		
 		
 		Sequence seq = document.createSequence(SequenceDisplayID, SequenceVersion, SequenceElements, SeqURI);
+		seq.setDescription("description");
+//		seq.setName("seq");
 		Sequence seq2 = (Sequence)document2.createCopy(seq, SequenceURI, SequenceDisplayID, seq2Version);
+		seq2.setDescription("description");
 		Sequence seq3 = (Sequence)document3.createCopy(seq, SequenceDisplayID);
 		
 		
@@ -70,7 +73,7 @@ public class SBOLDocumentTest {
 		assertTrue(seq.getClass().equals(seq2.getClass()));
 		assertTrue(seq.getAnnotations().equals(seq2.getAnnotations()));
 //		assertTrue(seq.getIdentity().equals(seq2.getIdentity()));						//assertion error
-//		assertTrue(seq.getDescription().equals(seq2.getDescription()));					//null pointer exception
+//		assertTrue(seq.getDescription().equals(seq2.getDescription()));					//null pointer exception ----------------------------------------------------------
 //		assertTrue(seq.getWasDerivedFrom().equals(seq2.getWasDerivedFrom()));			//null pointer exception
 		assertTrue(seq.getPersistentIdentity().equals(seq2.getPersistentIdentity()));	//assertion error
 //		assertTrue(seq.getName().equals(seq2.getName()));								//null pointer exception
@@ -115,6 +118,8 @@ public class SBOLDocumentTest {
 		URI model1URI_F = URI.create(F_model1URI);
 		
 		Model mod1 = document1.createModel(model1ID, model1Version, model1URI_S, model1URI_L, model1URI_F);
+		mod1.setName("mod1");
+		mod1.setDescription("description");
 //		Model mod2 = (Model)document2.createCopy(mod1);
 		Model mod3 = (Model)document2.createCopy(mod1, model1URIPrefix, model1ID, model1Version);
 		
@@ -123,13 +128,13 @@ public class SBOLDocumentTest {
 //		assertTrue(mod1.equals(mod2));	
 		assertTrue(mod1.getAnnotations().equals(mod3.getAnnotations()));
 		assertTrue(mod1.getClass().equals(mod3.getClass()));
-//		assertTrue(mod1.getDescription().equals(mod3.getDescription()));				//null pointer exception
+		assertTrue(mod1.getDescription().equals(mod3.getDescription()));				
 		assertTrue(mod1.getDisplayId().equals(mod3.getDisplayId()));
 		assertTrue(mod1.getFramework().equals(mod3.getFramework()));
-		assertTrue(mod1.getIdentity().equals(mod3.getIdentity()));						//assertion error
+		assertTrue(mod1.getIdentity().equals(mod3.getIdentity()));						
 		assertTrue(mod1.getLanguage().equals(mod3.getLanguage()));
-//		assertTrue(mod1.getName().equals(mod3.getName()));								//null pointer exception
-		assertTrue(mod1.getPersistentIdentity().equals(mod3.getPersistentIdentity()));	//assertion error
+		assertTrue(mod1.getName().equals(mod3.getName()));								
+		assertTrue(mod1.getPersistentIdentity().equals(mod3.getPersistentIdentity()));	
 		assertTrue(mod1.getSource().equals(mod3.getSource()));
 		assertTrue(mod1.getVersion().equals(mod3.getVersion()));
 //		assertTrue(mod1.getWasDerivedFrom().equals(mod3.getWasDerivedFrom()));			//null pointer exception
@@ -143,23 +148,37 @@ public class SBOLDocumentTest {
 		String prPrefix="pr";
 		SBOLDocument document1 = new SBOLDocument();
 		document1.setDefaultURIprefix(prURI);
-		document1.setTypesInURIs(true);
+		document1.setTypesInURIs(false);
 		
 		String prURI2="http://partsregistry2.org";
 		String prPrefix2="pr2";
 		SBOLDocument document2 = new SBOLDocument();
 		document2.setDefaultURIprefix(prURI2);
-		document2.setTypesInURIs(true);
+		document2.setTypesInURIs(false);
 		
 		String GTL1ID = "ID";
+		String GTL1Name = "GTL1";
+		String GTL1Description = "Description";
 		String GTL1Version = "1.0";
 		String GTL1Qname = "name";
 		GenericTopLevel GTL1 = document1.createGenericTopLevel(GTL1ID, GTL1Version, QName.valueOf(GTL1Qname));
+		GTL1.setDescription(GTL1Description);
+		GTL1.setName(GTL1Name);
 		GenericTopLevel GTL2 = (GenericTopLevel)document2.createCopy(GTL1);
 		
 		GTL2.unsetWasDerivedFrom();
 		
-		assertTrue(GTL1.equals(GTL2));
+		assertTrue(GTL1.equals(GTL2));	
+		assertTrue(GTL1.getAnnotations().equals(GTL2.getAnnotations()));
+		assertTrue(GTL1.getClass().equals(GTL2.getClass()));
+		assertTrue(GTL1.getDescription().equals(GTL2.getDescription()));			
+		assertTrue(GTL1.getDisplayId().equals(GTL2.getDisplayId()));
+		assertTrue(GTL1.getIdentity().equals(GTL2.getIdentity()));						
+		assertTrue(GTL1.getName().equals(GTL2.getName()));								
+		assertTrue(GTL1.getPersistentIdentity().equals(GTL2.getPersistentIdentity()));	
+		assertTrue(GTL1.getVersion().equals(GTL2.getVersion()));
+//		assertTrue(GTL1.getWasDerivedFrom().equals(mod3.getWasDerivedFrom()));			//null pointer exception
+		assertTrue(GTL1.getRDFType().equals(GTL2.getRDFType()));
 	}
 	
 	@Test
@@ -169,24 +188,26 @@ public class SBOLDocumentTest {
 		String prPrefix="pr";
 		SBOLDocument document1 = new SBOLDocument();
 		document1.setDefaultURIprefix(prURI);
-		document1.setTypesInURIs(true);
+		document1.setTypesInURIs(false);
 		
 		String prURI2="http://partsregistry2.org";
 		String prPrefix2="pr2";
 		SBOLDocument document2 = new SBOLDocument();
 		document2.setDefaultURIprefix(prURI2);
-		document2.setTypesInURIs(true);
+		document2.setTypesInURIs(false);
 		
 		String prURI3="http://partsregistry3.org";
 		String prPrefix3="pr3";
 		SBOLDocument document3 = new SBOLDocument();
 		document3.setDefaultURIprefix(prURI3);
-		document3.setTypesInURIs(true);
+		document3.setTypesInURIs(false);
 		
 		String Col1ID = "ID";
 		String Col3ID = "ID3";
 		String Col1Version = "1.0";
 		Collection Col1 = document1.createCollection(Col1ID, Col1Version);
+//		Col1.setDescription("description");
+//		Col1.setName("Col1");
 		Collection Col2 = (Collection)document2.createCopy(Col1);
 //		Collection Col3 = (Collection)document3.createCopy(Col1, Col1ID);
 		
@@ -195,6 +216,13 @@ public class SBOLDocumentTest {
 		
 //		assertTrue(Col1.equals(Col3));
 		assertTrue(Col1.equals(Col2));
+		assertTrue(Col1.getAnnotations().equals(Col2.getAnnotations()));
+		assertTrue(Col1.getClass().equals(Col2.getClass()));
+//		assertTrue(Col1.getDescription().equals(Col2.getDescription())); ----------------------------------------------------------
+//		assertTrue(Col1.getDisplayId().equals(Col2.getDisplayId()));   //display id should be equal
+//		assertTrue(Col1.getName().equals(Col2.getName()));
+		assertTrue(Col1.getPersistentIdentity().equals(Col2.getPersistentIdentity()));
+		assertTrue(Col1.getVersion().equals(Col2.getVersion()));
 	}
 	
 	@Test
@@ -204,23 +232,37 @@ public class SBOLDocumentTest {
 		String prPrefix="pr";
 		SBOLDocument document1 = new SBOLDocument();
 		document1.setDefaultURIprefix(prURI);
-		document1.setTypesInURIs(true);
+		document1.setTypesInURIs(false);
 		
 		String prURI2="http://partsregistry2.org";
 		String prPrefix2="pr2";
 		SBOLDocument document2 = new SBOLDocument();
 		document2.setDefaultURIprefix(prURI2);
-		document2.setTypesInURIs(true);
+		document2.setTypesInURIs(false);
 		
 		String CD1_ID = "ID";
 		Set<URI> types = new HashSet<URI>();
 		types.add(URI.create("www.example.com"));
 		ComponentDefinition CD1 = document1.createComponentDefinition(CD1_ID, types);
+		CD1.unsetDescription();
+		CD1.setDescription("Description");
+		CD1.unsetName();
+		CD1.setName("CD1");
 		ComponentDefinition CD2 = (ComponentDefinition)document2.createCopy(CD1);
 
 		CD2.unsetWasDerivedFrom();
 		
 		assertTrue(CD1.equals(CD2));
+		assertTrue(CD1.getAnnotations().equals(CD2.getAnnotations()));
+		assertTrue(CD1.getClass().equals(CD2.getClass()));
+		assertTrue(CD1.getComponents().equals(CD1.getComponents()));
+		assertTrue(CD1.getDescription().equals(CD1.getDescription()));
+		assertTrue(CD1.getDisplayId().equals(CD2.getDisplayId()));
+		assertTrue(CD1.getIdentity().equals(CD2.getIdentity()));
+		assertTrue(CD1.getName().equals(CD2.getName()));
+		assertTrue(CD1.getPersistentIdentity().equals(CD2.getPersistentIdentity()));
+		assertTrue(CD1.getSequences().equals(CD2.getSequences()));
+		assertTrue(CD1.getTypes().equals(CD2.getTypes()));
 	}
 	
 	@Test
@@ -230,22 +272,37 @@ public class SBOLDocumentTest {
 		String prPrefix="pr";
 		SBOLDocument document1 = new SBOLDocument();
 		document1.setDefaultURIprefix(prURI);
-		document1.setTypesInURIs(true);
+		document1.setTypesInURIs(false);
 		
 		String prURI2="http://partsregistry2.org";
 		String prPrefix2="pr2";
 		SBOLDocument document2 = new SBOLDocument();
 		document2.setDefaultURIprefix(prURI2);
-		document2.setTypesInURIs(true);
+		document2.setTypesInURIs(false);
 		
 		String MD1_ID = "ID";
 		String MD1_Version = "1.0";
+		String FuncID = "FunctionalComponentID";
+		String FuncDefID = "FuctionDefinitionID";
 		ModuleDefinition MD1 = document1.createModuleDefinition(MD1_ID, MD1_Version);
+		MD1.unsetDescription();
+		MD1.setDescription("Description");
+		MD1.createFunctionalComponent(FuncID, AccessType.PUBLIC, FuncDefID, MD1_Version, DirectionType.IN);
+		MD1.setName("MD1");
 		ModuleDefinition MD2 = (ModuleDefinition)document2.createCopy(MD1);
 		
 		MD2.unsetWasDerivedFrom();
 		
 		assertTrue(MD1.equals(MD2));
+		assertTrue(MD1.getAnnotations().equals(MD2.getAnnotations()));
+		assertTrue(MD1.getClass().equals(MD2.getClass()));
+		assertTrue(MD1.getDescription().equals(MD2.getDescription()));
+		assertTrue(MD1.getDisplayId().equals(MD2.getDisplayId()));
+//		assertTrue(MD1.getFunctionalComponent(FuncDefID).equals(MD2.getFunctionalComponent(FuncDefID)));   //do functional components not get copied?
+		assertTrue(MD1.getModels().equals(MD2.getModels()));
+		assertTrue(MD1.getName().equals(MD2.getName()));
+		assertTrue(MD1.getPersistentIdentity().equals(MD2.getPersistentIdentity()));
+		assertTrue(MD1.getVersion().equals(MD2.getVersion()));
 	}
 	
 	
