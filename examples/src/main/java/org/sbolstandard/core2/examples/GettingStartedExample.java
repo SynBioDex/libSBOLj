4d602743d0c86fa02f5218d9e0fd5831d0dfaa7e
Zhen Zhang <moschino21@gmail.com>
2015-08-17 04:43:12 -0600

    Modified core2/examples/GettingStartedExample.java.

--- examples/src/main/java/org/sbolstandard/core2/examples/GettingStartedExample.java
+++ examples/src/main/java/org/sbolstandard/core2/examples/GettingStartedExample.java
@@ -7,7 +7,9 @@ import java.util.HashSet;
 import org.sbolstandard.core2.ComponentDefinition;
 import org.sbolstandard.core2.RestrictionType;
 import org.sbolstandard.core2.SBOLDocument;
+import org.sbolstandard.core2.SBOLWriter;
 import org.sbolstandard.core2.Sequence;
+import org.sbolstandard.core2.SequenceConstraint;
 import org.sbolstandard.core2.SequenceOntology;
 
 /**
@@ -25,10 +27,9 @@ public class GettingStartedExample {
 		document.setCreateDefaults(true);
 		
 		// Creating a Top-level SBOL Data Object
-		URI type_chebi4705 = URI.create("http://identifiers.org/chebi/CHEBI:4705");
 		HashSet<URI> types = new HashSet<URI>(Arrays.asList(
 						ComponentDefinition.DNA,
-						type_chebi4705											
+						URI.create("http://identifiers.org/chebi/CHEBI:4705")
 						));		
 		ComponentDefinition TetR_promoter = document.createComponentDefinition(
 				"BBa_R0040", 
@@ -48,23 +49,29 @@ public class GettingStartedExample {
 				"tccctatcagtgatagagattgacatccctatcagtgatagagatactgagcac",
 				URI.create("http://dx.doi.org/10.1021/bi00822a023")
 				);
-		String element2 = "atggtgaatgtgaaaccagtaacgttatacgatgtcgcagagtatgccggtgtctcttatcagaccgtttcccgcgtg"
-				+ "gtgaaccaggccagccacgtttctgcgaaaacgcgggaaaaagtggaagcggcgatggcggagctgaattacattcccaaccgcgt"
-				+ "ggcacaacaactggcgggcaaacagtcgttgctgattggcgttgccacctccagtctggccctgcacgcgccgtcgcaaattgtcg"
-				+ "cggcgattaaatctcgcgccgatcaactgggtgccagcgtggtggtgtcgatggtagaacgaagcggcgtcgaagcctgtaaagcg"
-				+ "gcggtgcacaatcttctcgcgcaacgcgtcagtgggctgatcattaactatccgctggatgaccaggatgccattgctgtggaagc"
-				+ "tgcctgcactaatgttccggcgttatttcttgatgtctctgaccagacacccatcaacagtattattttctcccatgaagacggta"
-				+ "cgcgactgggcgtggagcatctggtcgcattgggtcaccagcaaatcgcgctgttagcgggcccattaagttctgtctcggcgcgt"
-				+ "ctgcgtctggctggctggcataaatatctcactcgcaatcaaattcagccgatagcggaacgggaaggcgactggagtgccatgtc"
-				+ "cggttttcaacaaaccatgcaaatgctgaatgagggcatcgttcccactgcgatgctggttgccaacgatcagatggcgctgggcg"
-				+ "caatgcgcgccattaccgagtccgggctgcgcgttggtgcggatatctcggtagtgggatacgacgataccgaagacagctcatgt"
-				+ "tatatcccgccgttaaccaccatcaaacaggattttcgcctgctggggcaaaccagcgtggaccgcttgctgcaactctctcaggg"
-				+ "ccaggcggtgaagggcaatcagctgttgcccgtctcactggtgaaaagaaaaaccaccctggcgcccaatacgcaaaccgcctctc"
-				+ "cccgcgcgttggccgattcattaatgcagctggcacgacaggtttcccgactggaaagcgggcaggctgcaaacgacgaaaactac"
-				+ "gctttagtagcttaataa";
+		String element2 = "atggtgaatgtgaaaccagtaacgttatacgatgtcgcagagtatgccggtgtc"
+				+ "tcttatcagaccgtttcccgcgtggtgaaccaggccagccacgtttctgcgaaaacgcggga"
+				+ "aaaagtggaagcggcgatggcggagctgaattacattcccaaccgcgtggcacaacaactgg"
+				+ "cgggcaaacagtcgttgctgattggcgttgccacctccagtctggccctgcacgcgccgtcg"
+				+ "caaattgtcgcggcgattaaatctcgcgccgatcaactgggtgccagcgtggtggtgtcgat"
+				+ "ggtagaacgaagcggcgtcgaagcctgtaaagcggcggtgcacaatcttctcgcgcaacgcg"
+				+ "tcagtgggctgatcattaactatccgctggatgaccaggatgccattgctgtggaagctgcc"
+				+ "tgcactaatgttccggcgttatttcttgatgtctctgaccagacacccatcaacagtattat"
+				+ "tttctcccatgaagacggtacgcgactgggcgtggagcatctggtcgcattgggtcaccagc"
+				+ "aaatcgcgctgttagcgggcccattaagttctgtctcggcgcgtctgcgtctggctggctgg"
+				+ "cataaatatctcactcgcaatcaaattcagccgatagcggaacgggaaggcgactggagtgc"
+				+ "catgtccggttttcaacaaaccatgcaaatgctgaatgagggcatcgttcccactgcgatgc"
+				+ "tggttgccaacgatcagatggcgctgggcgcaatgcgcgccattaccgagtccgggctgcgc"
+				+ "gttggtgcggatatctcggtagtgggatacgacgataccgaagacagctcatgttatatccc"
+				+ "gccgttaaccaccatcaaacaggattttcgcctgctggggcaaaccagcgtggaccgcttgc"
+				+ "tgcaactctctcagggccaggcggtgaagggcaatcagctgttgcccgtctcactggtgaaa"
+				+ "agaaaaaccaccctggcgcccaatacgcaaaccgcctctccccgcgcgttggccgattcatt"
+				+ "aatgcagctggcacgacaggtttcccgactggaaagcgggcaggctgcaaacgacgaaaact"
+				+ "acgctttagtagcttaataa";
+				
 		Sequence seq_153 = document.createSequence(
 				"partseq_153",
-				"",
+				"2.0",
 				element2,
 				URI.create("http://dx.doi.org/10.1021/bi00822a023")
 				);
@@ -74,18 +81,25 @@ public class GettingStartedExample {
 		if (TetR_promoter.isSetName()) {
 			TetR_promoter.unsetName();
 		}
-		System.out.println("TetR promoter " + TetR_promoter.getDisplayId() 
-				+ " has a name: " + TetR_promoter.getName());
 		TetR_promoter.addRole(SequenceOntology.PROMOTER);		
 		URI TetR_promoter_role2 = URI.create("http://identifiers.org/so/SO:0000613"); 
 		TetR_promoter.addRole(TetR_promoter_role2);
 		if (TetR_promoter.containsRole(TetR_promoter_role2)) {
 			TetR_promoter.removeRole(TetR_promoter_role2);
 		}
-		if (!TetR_promoter.containsRole(TetR_promoter_role2)) {
-			System.out.println("TetR promoter " + TetR_promoter.getDisplayId()
-					+ " does not contain role " + TetR_promoter_role2.toString());
+		if (TetR_promoter.containsRole(SequenceOntology.PROMOTER)) {
+			TetR_promoter.removeRole(SequenceOntology.PROMOTER);
 		}
+		TetR_promoter.clearRoles();
+		if (TetR_promoter.getRoles().isEmpty()) {
+			System.out.println("TetR_promoter's set of roles is empty");
+		}
+		TetR_promoter.setRoles(							
+					new HashSet<URI>(Arrays.asList(
+					SequenceOntology.PROMOTER,
+					TetR_promoter_role2)
+					));
+		
 		TetR_promoter.setWasDerivedFrom(URI.create("http://partsregistry.org/part/BBa_R0040"));
 		TetR_promoter.setDescription("TetR repressible promoter");
 		
@@ -93,13 +107,13 @@ public class GettingStartedExample {
 		LacI_repressor.setWasDerivedFrom(URI.create("http://partsregistry.org/part/BBa_C0012"));
 		LacI_repressor.setDescription("lacI repressor from E. coli (+LVA)");
 		LacI_repressor.setName("lacI");
-		
+
 		seq_187.setWasDerivedFrom(URI.create("http://partsregistry.org/seq/partseq_187"));
 		seq_153.setWasDerivedFrom(URI.create("http://partsregistry.org/seq/partseq_153"));
 		
 		// Creating and editing references
 		TetR_promoter.addSequence(seq_187);		
-		LacI_repressor.addSequence(seq_153);
+		LacI_repressor.addSequence(seq_153);		
 		pIKELeftCassette.addSequence(seq_187);
 		pIKELeftCassette.clearSequences();
 		
@@ -107,27 +121,35 @@ public class GettingStartedExample {
 		// For pIKELeftCassette, create sequence constraint that says BBa_R0040 precedes BBa_C0012.
 		// Note that with CreateDefaults that components get created automatically.
 		// The position of the subject Component MUST precede that of the object Component. 
-		pIKELeftCassette.createSequenceConstraint(
+		SequenceConstraint seqConstraint = pIKELeftCassette.createSequenceConstraint(
 				"pIKELeftCassette_sc",
 				RestrictionType.PRECEDES,
 				TetR_promoter.getDisplayId(), 
 				LacI_repressor.getDisplayId()
 				);
-		//System.out.println(pIKELeftCassette.getComponent("BBa_R0040"));
-		System.out.println(pIKELeftCassette.getComponent(TetR_promoter.getDisplayId()).getIdentity().toString());
-		// Removing the subject component below causes an exception. 
+		System.out.println(pIKELeftCassette.getComponent("BBa_R0040").getIdentity().toString());
+		//System.out.println(pIKELeftCassette.getComponent(TetR_promoter.getDisplayId()).getIdentity().toString());
+		System.out.println(pIKELeftCassette.getComponent("BBa_C0012").getIdentity().toString());
+		//System.out.println(seqConstraint.getSubject().getIdentity().toString());
+		
+		// Removing the subject component below causes an exception.		
 		//pIKELeftCassette.removeComponent(pIKELeftCassette.getComponent(TetR_promoter.getDisplayId()));
 		
 		// Copying objects
 		ComponentDefinition TetR_promoter_copy = (ComponentDefinition) document.createCopy(TetR_promoter, "BBa_K137046");
-//		Sequence seq_K137046 = document.createSequence(
-//				"seq_K137046",
-//				"",
-//				"gtgctcagtatctctatcactgatagggatgtcaatctctatcactgatagggactctagtatataaacgcagaaagg"
-//				+ "cccacccgaaggtgagccagtgtgactctagtagagagcgttcaccgacaaacaacagataaaacgaaaggc",
-//				URI.create("http://parts.igem.org/Part:BBa_K137046")
-//				);	
+		Sequence seq_K137046 = document.createSequence(
+				"seq_K137046",
+				"",
+				"gtgctcagtatctctatcactgatagggatgtcaatctctatcactgatagggactctagtatat"
+				+ "aaacgcagaaaggcccacccgaaggtgagccagtgtgactctagtagagagcgttcaccgaca"
+				+ "aacaacagataaaacgaaaggc",
+				URI.create("http://parts.igem.org/Part:BBa_K137046")
+				);	
 		TetR_promoter_copy.setSequences(
-				new HashSet<URI>(Arrays.asList(URI.create("http://parts.igem.org/Part:BBa_K137046"))));
+				new HashSet<URI>(Arrays.asList(
+						URI.create("http://parts.igem.org/Part:BBa_K137046")
+						)));
+		
+		SBOLWriter.write(document,(System.out));
 	}
 }
