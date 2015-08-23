ca7caf7bd7b1fada2c73095ca622a8bb50062961
Chris Myers <myers@ece.utah.edu>
2015-08-23 11:32:01 -0600

    Updated example code.

--- examples/src/main/java/org/sbolstandard/core2/examples/GettingStartedExample.java
+++ examples/src/main/java/org/sbolstandard/core2/examples/GettingStartedExample.java
@@ -32,21 +32,17 @@ public class GettingStartedExample {
 						));		
 		ComponentDefinition TetR_promoter = document.createComponentDefinition(
 				"BBa_R0040", 
-				"", 
 				types);
 		ComponentDefinition LacI_repressor = document.createComponentDefinition(
 				"BBa_C0012", 
-				"", 
 				types);
 		ComponentDefinition pIKELeftCassette = document.createComponentDefinition(
 				"pIKELeftCassette", 
-				"", 
 				types);
 		Sequence seq_187 = document.createSequence(
 				"partseq_187",
-				"",
 				"tccctatcagtgatagagattgacatccctatcagtgatagagatactgagcac",
-				URI.create("http://dx.doi.org/10.1021/bi00822a023")
+				Sequence.IUPAC_DNA
 				);
 		String element2 = "atggtgaatgtgaaaccagtaacgttatacgatgtcgcagagtatgccggtgtc"
 				+ "tcttatcagaccgtttcccgcgtggtgaaccaggccagccacgtttctgcgaaaacgcggga"
@@ -70,51 +66,42 @@ public class GettingStartedExample {
 				
 		Sequence seq_153 = document.createSequence(
 				"partseq_153",
-				"2.0",
 				element2,
-				URI.create("http://dx.doi.org/10.1021/bi00822a023")
+				Sequence.IUPAC_DNA
 				);
 		
 		// Setting and editing optional fields
 		TetR_promoter.setName("p(tetR)");
+		LacI_repressor.setName("lacI");
+		TetR_promoter.setDescription("TetR repressible promoter");
+		LacI_repressor.setDescription("lacI repressor from E. coli (+LVA)");
 		if (TetR_promoter.isSetName()) {
 			TetR_promoter.unsetName();
 		}
+		TetR_promoter.setName("p(tetR)");
+
 		TetR_promoter.addRole(SequenceOntology.PROMOTER);		
+		LacI_repressor.addRole(SequenceOntology.CDS);
 		URI TetR_promoter_role2 = URI.create("http://identifiers.org/so/SO:0000613"); 
 		TetR_promoter.addRole(TetR_promoter_role2);
 		if (TetR_promoter.containsRole(TetR_promoter_role2)) {
 			TetR_promoter.removeRole(TetR_promoter_role2);
 		}
-		if (TetR_promoter.containsRole(SequenceOntology.PROMOTER)) {
-			TetR_promoter.removeRole(SequenceOntology.PROMOTER);
-		}
-		TetR_promoter.clearRoles();
-		if (TetR_promoter.getRoles().isEmpty()) {
-			System.out.println("TetR_promoter's set of roles is empty");
-		}
-		TetR_promoter.setRoles(							
-					new HashSet<URI>(Arrays.asList(
-					SequenceOntology.PROMOTER,
-					TetR_promoter_role2)
-					));
-		
-		TetR_promoter.setWasDerivedFrom(URI.create("http://partsregistry.org/part/BBa_R0040"));
-		TetR_promoter.setDescription("TetR repressible promoter");
-		
-		LacI_repressor.addRole(URI.create("http://identifiers.org/so/SO:0000316"));
-		LacI_repressor.setWasDerivedFrom(URI.create("http://partsregistry.org/part/BBa_C0012"));
-		LacI_repressor.setDescription("lacI repressor from E. coli (+LVA)");
-		LacI_repressor.setName("lacI");
 
-		seq_187.setWasDerivedFrom(URI.create("http://partsregistry.org/seq/partseq_187"));
-		seq_153.setWasDerivedFrom(URI.create("http://partsregistry.org/seq/partseq_153"));
+		TetR_promoter.clearRoles();
+		if (!TetR_promoter.getRoles().isEmpty()) {
+			System.out.println("TetR_promoter set is not empty");
+		}
+		TetR_promoter.setRoles(new HashSet<URI>(Arrays.asList(
+					SequenceOntology.PROMOTER)));
 		
 		// Creating and editing references
 		TetR_promoter.addSequence(seq_187);		
 		LacI_repressor.addSequence(seq_153);		
 		pIKELeftCassette.addSequence(seq_187);
 		pIKELeftCassette.clearSequences();
+		// Adding the sequence below causes an exception because it cannot be found
+		//pIKELeftCassette.addSequence(URI.create("http://partsregistry.org/seq/partseq_154"));
 		
 		// Creating and editing Child Objects
 		// For pIKELeftCassette, create sequence constraint that says BBa_R0040 precedes BBa_C0012.
@@ -126,28 +113,25 @@ public class GettingStartedExample {
 				TetR_promoter.getDisplayId(), 
 				LacI_repressor.getDisplayId()
 				);
-		System.out.println(pIKELeftCassette.getComponent("BBa_R0040").getIdentity().toString());
-		//System.out.println(pIKELeftCassette.getComponent(TetR_promoter.getDisplayId()).getIdentity().toString());
-		System.out.println(pIKELeftCassette.getComponent("BBa_C0012").getIdentity().toString());
-		//System.out.println(seqConstraint.getSubject().getIdentity().toString());
-		
+		if (pIKELeftCassette.getComponent("BBa_R0040")==null) {
+			System.out.println("TetR_promoter component is missing");
+		}
+		if (pIKELeftCassette.getComponent("BBa_C0012")==null) {
+			System.out.println("LacI_repressor component is missing");
+		}
 		// Removing the subject component below causes an exception.		
-		//pIKELeftCassette.removeComponent(pIKELeftCassette.getComponent(TetR_promoter.getDisplayId()));
+		//pIKELeftCassette.removeComponent(pIKELeftCassette.getComponent("BBa_R0040"));
 		
 		// Copying objects
 		ComponentDefinition TetR_promoter_copy = (ComponentDefinition) document.createCopy(TetR_promoter, "BBa_K137046");
-		document.createSequence(
+		Sequence seq = document.createSequence(
 				"seq_K137046",
-				"",
 				"gtgctcagtatctctatcactgatagggatgtcaatctctatcactgatagggactctagtatat"
 				+ "aaacgcagaaaggcccacccgaaggtgagccagtgtgactctagtagagagcgttcaccgaca"
 				+ "aacaacagataaaacgaaaggc",
-				URI.create("http://parts.igem.org/Part:BBa_K137046")
+				Sequence.IUPAC_DNA
 				);	
-		TetR_promoter_copy.setSequences(
-				new HashSet<URI>(Arrays.asList(
-						URI.create("http://parts.igem.org/Part:BBa_K137046")
-						)));
+		TetR_promoter_copy.addSequence(seq);
 		
 		SBOLWriter.write(document,(System.out));
 	}
