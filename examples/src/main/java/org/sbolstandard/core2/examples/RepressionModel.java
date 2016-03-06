576f19d2b410557cb4493d733fdf1147c208f95a
Zhen Zhang <moschino21@gmail.com>
2016-03-06 02:42:19 -0700

    Corrected RepressionModel.java.

--- examples/src/main/java/org/sbolstandard/core2/examples/RepressionModel.java
+++ examples/src/main/java/org/sbolstandard/core2/examples/RepressionModel.java
@@ -374,9 +374,9 @@ public class RepressionModel {
 				datasheet.getIdentity());
 
 		ComponentDefinition pConst_alt = (ComponentDefinition) doc.createCopy(pConst, "pConst_alt");
-		pConst_alt.createAnnotation(
-				new QName(prURI, "", prPrefix),
-				URI.create("http://parts.igem.org/Part:BBa_J23100"));
+//		pConst_alt.createAnnotation(
+//				new QName(prURI, "", prPrefix),
+//				URI.create("http://parts.igem.org/Part:BBa_J23100"));
 		Sequence pConst_alt_seq = doc.createSequence("pConst_alt_seq", 
 											version, 
 											"ttgacggctagctcagtcctaggtacagtgctagc",
