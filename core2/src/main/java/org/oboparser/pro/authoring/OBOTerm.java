c1175a99177c5f4cbf53732566a540a8515e05ce
zgzn <moschino21@gmail.com>
2016-04-19 19:03:46 -0600

    Removed the unused subdirectory "pro" under oboparser.

--- core2/src/main/java/org/oboparser/pro/authoring/OBOTerm.java
+++
@@ -1,9 +0,0 @@
-package org.oboparser.pro.authoring;
-
-import java.io.*;
-
-public interface OBOTerm {
-	public void generateOBO(PrintStream ps);
-	public boolean inOBO();
-	public void clearInOBO();
-}
