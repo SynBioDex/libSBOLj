def580cee52ca423af95261d5bab2b3716185580
Zhen Zhang <moschino21@gmail.com>
2016-04-24 22:16:04 -0600

    Changed the visibility of SBOLValidationRule to package-only.

--- core2/src/main/java/org/sbolstandard/core2/SBOLValidationRule.java
+++ core2/src/main/java/org/sbolstandard/core2/SBOLValidationRule.java
@@ -9,7 +9,7 @@ package org.sbolstandard.core2;
  * @author Chris Myers
  * @version 2.0-beta
  */
-public class SBOLValidationRule {
+class SBOLValidationRule {
 
 	private String ruleClass;
 	private String id;
@@ -17,50 +17,50 @@ public class SBOLValidationRule {
 	private String description;
 	private String reference;
 
-	public SBOLValidationRule(String ruleClass) {
+	SBOLValidationRule(String ruleClass) {
 		this.ruleClass = ruleClass;
 		id = null;
 		condition = null;
 		description = null;
 	}
 
-	public String getRuleClass() {
+	String getRuleClass() {
 		return ruleClass;
 	}
 
-	public void setModelClass(String modelClass) {
+	void setModelClass(String modelClass) {
 		this.ruleClass = modelClass;
 	}
 
-	public String getId() {
+	String getId() {
 		return id;
 	}
 
-	public void setId(String id) {
+	void setId(String id) {
 		this.id = id;
 	}
 
-	public String getCondition() {
+	String getCondition() {
 		return condition;
 	}
 
-	public void setCondition(String condition) {
+	void setCondition(String condition) {
 		this.condition = condition;
 	}
 
-	public String getDescription() {
+	String getDescription() {
 		return description;
 	}
 
-	public void setDescription(String description) {
+	void setDescription(String description) {
 		this.description = description;
 	}
 	
-	public void setReference(String ref) {
+	void setReference(String ref) {
 		this.reference = ref;
 	}
 
-	public String getReference() {
+	String getReference() {
 		return reference;
 	}
 	
@@ -73,7 +73,7 @@ public class SBOLValidationRule {
 			+  "reference: " + this.reference+ "\n";
 	}
 	
-	public boolean isComplete() {
+	boolean isComplete() {
 //		if (this.getModelClass() == null) {
 //			return false;
 //		}
@@ -91,7 +91,7 @@ public class SBOLValidationRule {
 		}
 	}
 
-	public void setField(String field, String strBuff) {
+	void setField(String field, String strBuff) {
 		if (field.equals("id")) {
 			//currentRule.setId(strBuff);
 		}
@@ -104,7 +104,5 @@ public class SBOLValidationRule {
 		else if (field.equals("reference")) {
 			//currentRule.setReference(strBuff);
 		}
-	}
-	
-	
+	}	
 }
