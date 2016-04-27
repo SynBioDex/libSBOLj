61c626c5e01661207744837ae8b47803275222ca
eoberortner <eoberortner@gmail.com>
2016-04-27 08:06:49 -0700

    multi-line FASTA import

--- core2/src/test/java/org/sbolstandard/core2/FASTATest.java
+++ core2/src/test/java/org/sbolstandard/core2/FASTATest.java
@@ -66,26 +66,26 @@ public class FASTATest {
 			assertTrue(false);	// no exception allowed
 		}
 
-//		// two FASTA entries
-//		fasta = ">test1" + NEWLINE + 
-//				"acgt" + NEWLINE +
-//				">test2" + NEWLINE +
-//				"cgta";
-//		try (
-//				ByteArrayInputStream bais = 
-//					new ByteArrayInputStream(fasta.getBytes());
-//			) {
-//		
-//			SBOLDocument doc = FASTA.read(
-//					bais, URIprefix, "test", version, Sequence.IUPAC_DNA);
-//
-//			assertTrue(doc.getSequences().size() == 2);
-//
-//			// how can I retrieve a sequence nicely from the Document?
-//			assertTrue(null != doc.getSequence("test", version));
-//		} catch(Exception e) {
-//			assertTrue(false);	// no exception allowed
-//		}
+		// two FASTA entries
+		fasta = ">test1" + SBOLTestUtils.NEWLINE + 
+				"acgt" + SBOLTestUtils.NEWLINE +
+				">test2" + SBOLTestUtils.NEWLINE +
+				"cgta";
+		try (
+				ByteArrayInputStream bais = 
+					new ByteArrayInputStream(fasta.getBytes());
+			) {
+		
+			SBOLDocument doc = FASTA.read(
+					bais, URIprefix, "test", version, Sequence.IUPAC_DNA);
+
+			assertTrue(doc.getSequences().size() == 2);
+
+			// how can I retrieve a sequence nicely from the Document?
+			assertTrue(null != doc.getSequence("test", version));
+		} catch(Exception e) {
+			assertTrue(false);	// no exception allowed
+		}
 	
 		// one multi-line FASTA entry
 		fasta = ">test1" + SBOLTestUtils.NEWLINE + 
