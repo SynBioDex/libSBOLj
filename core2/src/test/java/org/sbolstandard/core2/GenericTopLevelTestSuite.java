18576a7ce4138faf925df5f146e91e57f4455ac8
mehersam <mehersam251@gmail.com>
2016-04-27 07:11:37 -0600

    added more testing suites for tests

---
+++ core2/src/test/java/org/sbolstandard/core2/GenericTopLevelTestSuite.java
@@ -0,0 +1,19 @@
+package org.sbolstandard.core2;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GenericTopLevelTestSuite {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+}
