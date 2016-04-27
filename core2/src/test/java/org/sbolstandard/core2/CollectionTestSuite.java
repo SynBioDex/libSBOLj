8c0891b2cbf3c934e91a7d986b7f14ae7c71a7dc
mehersam <mehersam251@gmail.com>
2016-04-27 07:09:02 -0600

    added testing suites to test library

---
+++ core2/src/test/java/org/sbolstandard/core2/CollectionTestSuite.java
@@ -0,0 +1,21 @@
+package org.sbolstandard.core2;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CollectionTestSuite {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+
+
+}
---
+++ core2/src/test/java/org/sbolstandard/core2/ComponentInstanceTestSuite.java
@@ -0,0 +1,20 @@
+package org.sbolstandard.core2;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ComponentInstanceTestSuite {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+
+}
