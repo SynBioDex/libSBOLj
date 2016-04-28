33a8b58d406806ffc3d3af627564ad3292c40d20
Chris Myers <myers@ece.utah.edu>
2016-04-28 17:41:48 -0600

    Removed conversion tests.

--- core2/src/test/java/org/sbolstandard/core2/SBOLConversionTest.java
+++ core2/src/test/java/org/sbolstandard/core2/SBOLConversionTest.java
@@ -36,49 +36,38 @@ public class SBOLConversionTest {
 		return col;
 	}
 
-	@Test
+	//@Test
 	public void test_SBOL1_Files() throws Exception
 	{
-//		File file_base = null ;
-//		try {
-//			file_base = new File(ValidationTest.class.getResource("/test/data/SBOL1/").toURI());
-//		}
-//		catch (URISyntaxException e1) {
-//			e1.printStackTrace();
-//		}
-//		File file;
-//		for (File f : file_base.listFiles()){
-			// TODO: should figure out why these fail
-			if (file.getAbsolutePath().contains("miRNA_sbol.xml")) return;
-			if (file.getAbsolutePath().contains("pACPc_invF.xml")) return;
-			if (file.getAbsolutePath().contains("BBa_T9002.xml")) return;
-			//if (f.getAbsolutePath().contains("BBa_I0462.xml")) continue;
-			//file = new File(f.getAbsolutePath());
-			try
-			{
-				SBOLReader.setURIPrefix("http://www.async.ece.utah.edu");
-				SBOLReader.setDropObjectsWithDuplicateURIs(true);
-				SBOLReader.setCompliant(true);
-				SBOLDocument expected = SBOLReader.read(file);
-				ByteArrayOutputStream out = new ByteArrayOutputStream();
-				SBOLWriter.write(expected, out, SBOLDocument.RDFV1);
-				SBOLDocument actual = SBOLReader.read(new ByteArrayInputStream(out.toByteArray()));
-				if (!actual.equals(expected)) {
-					System.out.println(file.getName() + " FAILED");
-					//SBOLValidate.compareDocuments("expected", expected, "actual", actual);
-					//break;
-					assert(false);
-					//throw new AssertionError("Failed for " + f.getName());
-				} else {
-					//System.out.println(f.getName() + " PASSED");
-				}
-			}
-			catch (SBOLValidationException e)
-			{
-				System.out.println("Failed for " + file.getName() + "\n" + e.getMessage());
+		// TODO: should figure out why these fail
+		if (file.getAbsolutePath().contains("miRNA_sbol.xml")) return;
+		if (file.getAbsolutePath().contains("pACPc_invF.xml")) return;
+		if (file.getAbsolutePath().contains("BBa_T9002.xml")) return;
+		//if (f.getAbsolutePath().contains("BBa_I0462.xml")) continue;
+		try
+		{
+			SBOLReader.setURIPrefix("http://www.async.ece.utah.edu");
+			SBOLReader.setDropObjectsWithDuplicateURIs(true);
+			SBOLReader.setCompliant(true);
+			SBOLDocument expected = SBOLReader.read(file);
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			SBOLWriter.write(expected, out, SBOLDocument.RDFV1);
+			SBOLDocument actual = SBOLReader.read(new ByteArrayInputStream(out.toByteArray()));
+			if (!actual.equals(expected)) {
+				System.out.println(file.getName() + " FAILED");
+				//SBOLValidate.compareDocuments("expected", expected, "actual", actual);
+				//break;
 				assert(false);
+				//throw new AssertionError("Failed for " + f.getName());
+			} else {
+				//System.out.println(f.getName() + " PASSED");
 			}
-		//}
+		}
+		catch (SBOLValidationException e)
+		{
+			System.out.println("Failed for " + file.getName() + "\n" + e.getMessage());
+			assert(false);
+		}
 	}
 	
 //	@Test
