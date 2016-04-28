7523c512a42f4dcc9292ba1373f1b9b36bab80ff
Chris Myers <myers@ece.utah.edu>
2016-04-28 17:45:59 -0600

    Remove conversion tests.

--- core2/src/test/java/org/sbolstandard/core2/SBOLConversionTest.java
+++ core2/src/test/java/org/sbolstandard/core2/SBOLConversionTest.java
@@ -11,7 +11,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
-@RunWith(Parameterized.class)
+//@RunWith(Parameterized.class)
 public class SBOLConversionTest {
 	
 	private File file;
