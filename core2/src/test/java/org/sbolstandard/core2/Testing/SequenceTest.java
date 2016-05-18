e8365b4825e596a414df5f4523ee83863fe8b087
Meher Samineni <mehersam251@gmail.com>
2016-05-18 10:13:14 -0600

    tested Sequence deepCopy

--- core2/src/test/java/org/sbolstandard/core2/Testing/SequenceTest.java
+++ core2/src/test/java/org/sbolstandard/core2/Testing/SequenceTest.java
@@ -11,12 +11,15 @@ import org.junit.Before;
 import org.junit.Test;
 import org.sbolstandard.core2.ComponentDefinition;
 import org.sbolstandard.core2.SBOLDocument;
+import org.sbolstandard.core2.SBOLValidationException;
 import org.sbolstandard.core2.Sequence;
 
 public class SequenceTest {
 	private Sequence s2 = null;
 	private SBOLDocument doc = null;
 	private Sequence generic_seq = null;
+	private String CRa_U6_seq = "GGTTTACCGAGCTCTTATTGGTTTTCAAACTTCATTGACTGTGCC";
+
 	@Before
 	public void setUp() throws Exception 
 	{
@@ -33,7 +36,15 @@ public class SequenceTest {
 	public void tearDown() throws Exception {
 	}
 
-
+	@Test
+	public void test_deep_copy() throws SBOLValidationException
+	{
+		Sequence gen_seq = doc.createSequence("CRa_U6_seq", CRa_U6_seq, Sequence.IUPAC_DNA);
+		Sequence copy_seq = (Sequence) doc.createCopy(gen_seq, "copy_seq");
+		//assertTrue(copy_seq.equals(gen_seq));
+		assertTrue(copy_seq.getElements().equals(CRa_U6_seq));
+		
+	}
 	@Test
 	public void test_toString()
 	{	
