e18e32507924bb1b611d4162da2c7e6000b9b229
Meher Samineni <mehersam251@gmail.com>
2016-05-23 15:48:35 -0600

    added toString test to model and renamed class name so maven will run it

---
+++ core2/src/test/java/org/sbolstandard/core2/Testing/ModelTest.java
@@ -0,0 +1,51 @@
+package org.sbolstandard.core2.Testing;
+
+import static org.junit.Assert.*;
+
+import java.net.URI;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.sbolstandard.core2.Model;
+import org.sbolstandard.core2.SBOLDocument;
+import org.sbolstandard.core2.SystemsBiologyOntology;
+
+public class ModelTest {
+	
+	private SBOLDocument document = null;
+	private Model test_model = null;
+
+	@Before
+	public void setUp() throws Exception {
+		document = new SBOLDocument();				
+		document.setTypesInURIs(false);		
+		document.setDefaultURIprefix("http://www.sbolstandard.org/examples");
+		
+		test_model=document.createModel(
+				"pIKE_Toggle_1",
+				"",
+				URI.create("http://virtualparts.org/part/pIKE_Toggle_1"),
+				URI.create("http://identifiers.org/edam/format_2585"), 
+				SystemsBiologyOntology.CONTINUOUS_FRAMEWORK);
+		test_model.setName("pIKE_Toggle_1 toggle switch");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+
+	@Test
+	public void test_toString()
+	{
+		String model = test_model.toString();
+		assertTrue(model.length() != 0);
+		assertNotNull(model);
+		assertTrue(model.contains("name=" + test_model.getName()));
+		assertTrue(!model.contains("description=" + test_model.getDescription()));
+
+	}
+	
+
+}
