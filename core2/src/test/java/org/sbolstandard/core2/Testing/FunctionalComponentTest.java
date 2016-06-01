cc4a0f3f83069ef154fa9132b3e0871e88d22384
Meher Samineni <mehersam251@gmail.com>
2016-06-01 09:24:37 -0600

    still adding methods---incomplete

---
+++ core2/src/test/java/org/sbolstandard/core2/Testing/FunctionalComponentTest.java
@@ -0,0 +1,28 @@
+package org.sbolstandard.core2.Testing;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.sbolstandard.core2.ComponentDefinition;
+
+public class FunctionalComponentTest {
+	private ComponentDefinition gRNA_b_gene = null;
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+
+	@Test
+	public void test_addMapsTo()
+	{
+	}
+	
+
+}
