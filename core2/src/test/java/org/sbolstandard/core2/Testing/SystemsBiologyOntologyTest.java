4e9a04abc93726e625ae5630810b7d888e9a49ad
Meher Samineni <mehersam251@gmail.com>
2016-07-24 22:12:31 -0600

    added getDescendentNames() to SystemsBiologyOntology

--- core2/src/test/java/org/sbolstandard/core2/Testing/SystemsBiologyOntologyTest.java
+++ core2/src/test/java/org/sbolstandard/core2/Testing/SystemsBiologyOntologyTest.java
@@ -1,8 +1,12 @@
 package org.sbolstandard.core2.Testing;
 
 import static org.junit.Assert.*;
+
+import java.net.URISyntaxException;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.sbolstandard.core2.SequenceOntology;
 import org.sbolstandard.core2.SystemsBiologyOntology;
 
 public class SystemsBiologyOntologyTest {
@@ -22,6 +26,15 @@ public class SystemsBiologyOntologyTest {
 		assertTrue(descendents.getDescendantURIsOf("SBO:0000003").contains(SystemsBiologyOntology.SIDE_PRODUCT));
 		assertTrue(descendents.getDescendantURIsOf(SystemsBiologyOntology.PRODUCT).size() == 1);
 
+	}
+	
+	@Test
+	public void test_getDecendentNames() throws URISyntaxException
+	{
+		assertTrue(descendents.getDescendantNamesOf(SystemsBiologyOntology.CATALYST).size() != 0);
+		assertTrue(descendents.getDescendantNamesOf(SystemsBiologyOntology.CATALYST).contains("enzymatic catalyst"));
+		assertTrue(descendents.getDescendantNamesOf("SBO:0000013").size() != 0);
+		assertTrue(descendents.getDescendantNamesOf("SBO:0000013").contains("enzymatic catalyst"));
 	}
 
 }
