db5da4ea4decde5107e76e735589980f0d30a5da
Chris Myers <myers@ece.utah.edu>
2017-11-29 08:14:07 -0700

    Copy wasGeneratedBys

--- core2/src/main/java/org/sbolstandard/core2/Identified.java
+++ core2/src/main/java/org/sbolstandard/core2/Identified.java
@@ -110,6 +110,9 @@ public abstract class Identified {
 		for (URI wasDerivedFrom : identified.getWasDerivedFroms()) {
 			this.addWasDerivedFrom(URI.create(wasDerivedFrom.toString()));
 		}
+		for (URI wasGeneratedBy : identified.getWasGeneratedBys()) {
+			this.addWasGeneratedBy(URI.create(wasGeneratedBy.toString()));
+		}
 		if (identified.isSetName()) {
 			this.setName(identified.getName());
 		}
