2185bb6f6462bbcd85bc979a82756095dcad08fc
Chris Myers <myers@ece.utah.edu>
2017-12-13 17:45:16 -0700

    Updated readTester

--- core2/src/test/java/org/sbolstandard/core2/readTester.java
+++ core2/src/test/java/org/sbolstandard/core2/readTester.java
@@ -40,14 +40,22 @@ class readTester {
 
 		try {
 			SBOLReader.setURIPrefix("http://dummy.org/");
-			SBOLDocument doc = SBOLReader.read("/Users/myers/git/SBOLTestRunner/SB_Tester/Emulated/toggle.xml_Emulated.xml");
-			SBOLDocument doc2 = SBOLReader.read("/Users/myers/git/SBOLTestRunner/SB_Tester/Retrieved/toggle.xml_Retrieved.xml");
-			SBOLValidate.compareDocuments("em", doc, "re", doc2);
-			if (SBOLValidate.getNumErrors() > 0) {
-				for (String error : SBOLValidate.getErrors()) {
-					System.out.println(error+"\n");
-				}	
-			}
+			SBOLReader.setCompliant(true);
+			SBOLDocument doc = SBOLReader.read("/Users/myers/git/SBOLTestRunner/SB_Tester/SBOLTestSuite/SBOL2/labhost_Bacillus_subtilis_orig.xml");
+			//SBOLDocument doc2 = new SBOLDocument();
+			doc.setDefaultURIprefix("http://dummy.org/");
+			//doc2.createCopy(doc);
+			//doc2.write(System.out);
+			doc = doc.changeURIPrefixVersion("http://dummy.org/", "1");
+			doc.write(System.out);
+//			SBOLDocument doc = SBOLReader.read("/Users/myers/git/SBOLTestRunner/SB_Tester/Emulated/toggle.xml_Emulated.xml");
+//			SBOLDocument doc2 = SBOLReader.read("/Users/myers/git/SBOLTestRunner/SB_Tester/Retrieved/toggle.xml_Retrieved.xml");
+//			SBOLValidate.compareDocuments("em", doc, "re", doc2);
+//			if (SBOLValidate.getNumErrors() > 0) {
+//				for (String error : SBOLValidate.getErrors()) {
+//					System.out.println(error);
+//				}	
+//			}
 			//SBOLDocument doc = SBOLReader.read("/Users/myers/Downloads/bug.xml");
 			//doc = doc.changeURIPrefixVersion("http://new.org/", "1.0");
 			//doc.write(System.out);
