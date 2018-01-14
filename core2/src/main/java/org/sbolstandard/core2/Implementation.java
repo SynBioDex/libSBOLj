a84afc68276c86705254f1971891a33028b6ab12
IgorDurovic <id.tesla@gmail.com>
2018-01-14 03:43:47 -0700

    optional built field added along with isSetBuilt() method

--- core2/src/main/java/org/sbolstandard/core2/Implementation.java
+++ core2/src/main/java/org/sbolstandard/core2/Implementation.java
@@ -3,11 +3,22 @@ package org.sbolstandard.core2;
 import java.net.URI;
 
 public class Implementation extends TopLevel {
+	
+	private URI built;
 
 	Implementation(URI identity) throws SBOLValidationException {
 		super(identity);
 		
 		// TODO Auto-generated constructor stub
+	}
+	
+	/**
+	 * Checks if the built property is set.
+	 * 
+	 * @return {@code true} if it is not {@code null}, {@code false} otherwise
+	 */
+	public boolean isSetBuilt() {
+		return built != null;
 	}
 
 	@Override
