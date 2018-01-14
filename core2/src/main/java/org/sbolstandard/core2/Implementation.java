8cbe263ad13842dea51231ab2da85599eb01cb41
IgorDurovic <id.tesla@gmail.com>
2018-01-14 03:26:46 -0700

    Added TopLevel abstract methods to Implementation

--- core2/src/main/java/org/sbolstandard/core2/Implementation.java
+++ core2/src/main/java/org/sbolstandard/core2/Implementation.java
@@ -1,5 +1,31 @@
 package org.sbolstandard.core2;
 
-public class Implementation {
+import java.net.URI;
+
+public class Implementation extends TopLevel {
+
+	Implementation(URI identity) throws SBOLValidationException {
+		super(identity);
+		
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	Identified deepCopy() throws SBOLValidationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	Identified copy(String URIprefix, String displayId, String version) throws SBOLValidationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	void checkDescendantsURIcompliance() throws SBOLValidationException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
