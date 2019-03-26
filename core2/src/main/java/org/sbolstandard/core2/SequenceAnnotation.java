c4cd0f8df7d011405fa968c7c8a3f1a3a8ce0c25
Chris Myers <myers@ece.utah.edu>
2019-03-26 13:23:14 -0600

    Fixed null pointer exception

--- core2/src/main/java/org/sbolstandard/core2/SequenceAnnotation.java
+++ core2/src/main/java/org/sbolstandard/core2/SequenceAnnotation.java
@@ -278,7 +278,8 @@ public class SequenceAnnotation extends Identified implements Comparable<Sequenc
 		addChildSafely(location, locations, "location");
 		location.setSBOLDocument(this.getSBOLDocument());
 		location.setComponentDefinition(componentDefinition);
-		if (location.isSetSequence() && !componentDefinition.getSequenceURIs().contains(location.getSequence())) {
+		if (location.isSetSequence() && componentDefinition != null && 
+				!componentDefinition.getSequenceURIs().contains(location.getSequence())) {
 			throw new SBOLValidationException("sbol-11003",this);
 		}
 	}
